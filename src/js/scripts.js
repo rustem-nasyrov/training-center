051876f6d7fd241a353a7af35920cc49767d9bf6
Rustem Nasyrov <nasyrov.rustem.r@ya.ru>
2019-11-17 13:34:28 +0500

    A few changes, moved scripts to separate file

---
+++ src/js/scripts.js
@@ -0,0 +1,62 @@
+'use strict';
+$(function () {
+    let $slider = $('.slider').slick({
+        autoPlay: true,
+        dots: false,
+        prevArrow: $('.slider-btn-prev'),
+        nextArrow: $('.slider-btn-next'),
+    });
+    $('.slider-paging__current').text($(
+        '.slider-item:not(.slick-cloned)')
+        .index());
+    $('.slider-paging__total').text($(
+        '.slider-item:not(.slick-cloned)').length);
+    const pageCounterHandler = (event, slick,
+        currentSlide, nextSlide) => {
+        let i = (currentSlide ? currentSlide : 0) + 1;
+        $('.slider-paging__current').text(i);
+        $('.slider-paging__total').text(slick.slideCount);
+    }
+    $slider.on('init reInit afterChange', pageCounterHandler);
+    $('.auth-btn').click(() => $('.auth-popup').toggleClass(
+        'auth-popup--active'));
+    $('.nav-btn').click(() => {
+        $('.header-nav-container').toggleClass(
+            'header-nav-container--active');
+    });
+    $(window).on('scroll', function () {
+        let scroll = $(window).scrollTop();
+        if (scroll > 50)
+            $('.header').addClass('header--fixed');
+        else
+            $('.header').removeClass('header--fixed');
+
+    });
+    $('.form-item__input').on('focusin', function () {
+        $(this).parent().addClass('form-item--active');
+    }).on('focusout', function () {
+        $(this).parent().removeClass('form-item--active');
+    });
+    $('.reviews-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },],
+    });
+    $('.psycho-slider').slick({
+        autoplay: true,
+        delay: 3000,
+        dots: false,
+        slidesToShow: 1,
+        arrows: true,
+        prevArrow: $('.psycho-slider-controls__prev'),
+        nextArrow: $('.psycho-slider-controls__next'),
+    });
+});
